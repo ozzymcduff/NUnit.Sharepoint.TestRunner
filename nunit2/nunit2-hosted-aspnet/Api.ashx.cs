a226f78645032457658f6f96fc213b0a968afe95
Oskar Gewalli <oskar@gewalli.se>
2015-12-03 18:33:37 +0100

    structure of the project

---
+++ nunit2/nunit2-hosted-aspnet/Api.ashx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+
+namespace nunit2hostedaspnet
+{
+    
+    public class Api : System.Web.IHttpHandler
+    {
+    
+        public void ProcessRequest (HttpContext context)
+        {
+            
+        }
+
+        public bool IsReusable {
+            get {
+                return false;
+            }
+        }
+    }
+}
+    
