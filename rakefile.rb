fb88a06ef800034c9bfa7912c25336d8f751cd8a
Oskar Gewalli <gewalli@gmail.com>
2015-12-31 22:29:10 +0100

    rake

---
+++ rakefile.rb
@@ -0,0 +1,34 @@
+require 'albacore'
+require 'nuget_helper'
+task :default => [:build]
+dir = File.dirname(__FILE__)
+
+desc "Install missing NuGet packages."
+task :restore do
+  NugetHelper.exec("restore nunit2.sln")
+  NugetHelper.exec("restore nunit3.sln")
+end
+
+desc "build solution"
+build :build_nunit2 do |msb, args|
+  msb.prop :configuration, :Debug
+  msb.target = [:Rebuild]
+  msb.sln = "nunit2.sln"
+end
+
+desc "build solution"
+build :build_nunit3 do |msb, args|
+  msb.prop :configuration, :Debug
+  msb.target = [:Rebuild]
+  msb.sln = "nunit3.sln"
+end
+
+task :build => [:build_nunit2, :build_nunit3]
+
+desc "test using console"
+test_runner :test => [:build] do |runner|
+  runner.exe = NugetHelper::nunit_path
+  files = Dir.glob(File.join(dir, "**", "bin", "Debug", "*Tests.dll"))
+  runner.files = files 
+end
+
